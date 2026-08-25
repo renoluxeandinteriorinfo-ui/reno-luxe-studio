f4b739c73091d32cc621c6c945eb1e59df1b7234
renoluxeandinteriorinfo-ui <renoluxeandinterior.info@gmail.com>
2026-08-25 18:16:47 +0100

    Update catalog.ts

--- src/lib/catalog.ts
+++ src/lib/catalog.ts
@@ -11,7 +11,7 @@
 "/kaws.jpg";
 "/visualization-lamp.jpg";
 "/back-man-figurines.jpg";
-export const swingChairs = [ ... ];
+export const swingChairs = [{ id: 1, name: "Swing Chair", image: "/swing-chair.jpg" }];   
 "/frames.jpg";
 "/plastic-vase-flowers.jpg";
 "/mirror.jpg";
